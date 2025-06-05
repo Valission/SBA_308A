f96334a7c0779cde59947b11080b71877dd797f2
Valission <FernandomvHull@gmail.com>
2025-06-05 10:07:27 -0400

    fetching_Data

--- API_Data.js
+++ API_Data.js
@@ -1,16 +1,11 @@
-import { selected_value } from "./User_Inter.js";
-export {characters}
-let url = "https://www.amiiboapi.com/api/amiibo/?name=mario&showusage";
-
-function urlChoice(name) {
-  url = `https://www.amiiboapi.com/api/amiibo/?name=${name}&showusage`;
+export async function getCharacters(name) {
+  const url = `https://www.amiiboapi.com/api/amiibo/?name=${name}&showusage`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.amiibo; // return full character data
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return [];
+  }
 }
-
-urlChoice(selected_value);
-
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    let characters = data.amiibo.map((item) => item.character);
-    console.log(characters);
-  });
